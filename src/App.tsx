0e761dd21f86c5d2814b4e62f2a15843f6fa684d
genival.junior <genivaljunior86@gmail.com>
2023-04-24 20:16:51 -0300

    Axios Implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,65 @@
-import React from 'react';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-
+import React, { useEffect, useState } from 'react';
+import { Breadcrumb, Layout, Menu, Table, theme } from 'antd';
+import axios from 'axios'
 const { Header, Content, Sider } = Layout;
+
+type ProductRow = {
+  id: number;
+  attributes: {
+    Name: string;
+    Price: number;
+    createdAt: string;
+    publishedAt: string;
+    updatedAt: string
+  }
+}
+
 
 const App: React.FC = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const [dataSource, setdataSource] = useState([]);
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
+      render: (value: number) => {
+        return `R$ ${value}`;
+      }
+    }
+  ];
+
+  useEffect(() => {
+    axios.get(
+      'http://localhost:1337/api/products',
+      {
+        headers: { Authorization: `Bearer 5e415dbea412d1b7d6739a9520f9ed2c8426b21ed08299fc5b9e1efdec49dd441b91ae2c8a4f1e3efdf513e39efaad4d9b8eca5cffe63094cd6bc86388e9f8ea0cc9f3091cc6bb6702a5c2c3802b9f452617347f755f6db9290161ec533dd72ab69adfeecc4c3cce8518e3640d4febd98418604b95f2c1ae091c5f374f6fec1d` }
+      }
+    )
+      .then(response => {
+        const { data: { data: dataResponse } } = response;
+        const dataList = dataResponse.map((row: ProductRow) => {
+          const { id, attributes: { Name: name, Price: price } } = row;
+          return {
+            key: id,
+            name,
+            price
+          }
+        });
+
+        setdataSource(dataList)
+      })
+  }, [])
+
 
   return (
     <Layout>
@@ -42,7 +95,7 @@ const App: React.FC = () => {
               background: colorBgContainer,
             }}
           >
-            Content
+            <Table dataSource={dataSource} columns={columns} />
           </Content>
         </Layout>
       </Layout>
