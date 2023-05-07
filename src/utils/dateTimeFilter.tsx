91226b01f0ac69e7dc875ecd0c90ff14eabd4478
genival.junior <genivaljunior86@gmail.com>
2023-05-07 20:34:08 -0300

    Date Filter

---
+++ src/utils/dateTimeFilter.tsx
@@ -0,0 +1,17 @@
+import dayjs from "dayjs";
+
+/**
+ * Filter to show date in Brazilian format
+ * @param value Date to be converted
+ * @returns Converted date. e.g: terça-feira, 23 de maio de 2023
+ */
+export default function dateTimeFilter(value: string, opts?: Object) {
+
+  return new Intl.DateTimeFormat('pt-BR', { ...opts, dateStyle: 'full' }).format(new Date(value));
+}
+/**
+ * Filter to send date in server format
+ * @param value Date to be converted
+ * @returns Converted date. e.g: 2023-05-31
+ */
+export const dateRequestFilter = (value: string) => dayjs(value).toISOString();
