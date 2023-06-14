064f737baea8d955b01d6eb8aa43e68a40c6f584
genivaljr86 <genivaljunior86@gmail.com>
2023-06-13 21:19:46 -0300

    OrderCreatePage: Remove Animation

--- src/pages/Order/List/index.tsx
+++ src/pages/Order/List/index.tsx
@@ -9,7 +9,6 @@ import { DollarCircleOutlined, ExclamationCircleFilled } from "@ant-design/icons
 import dateTimeFilter from "../../../utils/dateTimeFilter";
 import { AxiosError } from "axios";
 import useOrderListPageHooks from "./hooks";
-import { animated, useSpring } from "@react-spring/web";
 
 const columns: ColumnsType<any> = [
   {
@@ -45,10 +44,6 @@ const OrderListPage: React.FC = () => {
     Modal
   } = useOrderListPageHooks()
 
-  const springs = useSpring({
-    from: { y: -30, opacity: 0 },
-    to: { y: 0, opacity: 1 },
-  })
 
   async function fetchData() {
     setLoading(true);
@@ -144,19 +139,6 @@ const OrderListPage: React.FC = () => {
       <Table
         loading={loading}
         dataSource={dataSource}
-        components={
-          {
-            body: {
-              row: ({ ...props }) => {
-                const newProps = {
-                  ...props,
-                  style: springs
-                }
-                return <animated.tr {...newProps}></animated.tr>
-              }
-            }
-          }
-        }
         pagination={tableParams.pagination}
         onChange={handleTableChange}
         columns={columns} />
