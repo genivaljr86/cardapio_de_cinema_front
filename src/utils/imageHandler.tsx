26c5ee68896e08485bb88f29093290e58ff1a6fa
genivaljr86 <genivaljunior86@gmail.com>
2023-07-05 21:56:23 -0300

    imageHandler: Fix Defensive Code

--- src/utils/imageHandler.tsx
+++ src/utils/imageHandler.tsx
@@ -7,7 +7,7 @@ export type ImageFormatTypes = 'large' | 'medium' | 'small' | 'thumbnail';
 
 export default function imageHandler(imageResponse: Product["photo"], format?: ImageFormatTypes) {
   const photoAttributes = imageResponse?.data![0].attributes;
-  if (format && photoAttributes?.formats[format]) {
+  if (format && photoAttributes?.formats && photoAttributes?.formats[format]) {
     return devServerUrl + photoAttributes?.formats[format].url
   } else {
     return devServerUrl + photoAttributes?.url
